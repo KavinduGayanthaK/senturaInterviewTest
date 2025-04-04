590d476104766d5eed1dc5b6b2aed21cce76f05f
Kavindu Gayantha <117292977+KavinduGayanthaK@users.noreply.github.com>
2025-04-04 09:08:06 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/senturaInterviewTest/senturaInterviewTest/SenturaInterviewTestApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.senturaInterviewTest.senturaInterviewTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenturaInterviewTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenturaInterviewTestApplication.class, args);
+	}
+
+}
