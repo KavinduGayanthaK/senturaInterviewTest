58e3c9d9307b1fd694a1977da60721a5c7a2999d
Kavindu Gayantha <117292977+KavinduGayanthaK@users.noreply.github.com>
2025-04-04 09:39:19 +0530

    created save user controller

--- src/main/java/lk/ijse/senturaInterviewTest/senturaInterviewTest/controller/UserController.java
+++ src/main/java/lk/ijse/senturaInterviewTest/senturaInterviewTest/controller/UserController.java
@@ -1,13 +1,28 @@
 package lk.ijse.senturaInterviewTest.senturaInterviewTest.controller;
 
+import jakarta.validation.Valid;
+import lk.ijse.senturaInterviewTest.senturaInterviewTest.dto.UserDTO;
+import lk.ijse.senturaInterviewTest.senturaInterviewTest.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDTO> saveUser(@Valid @RequestBody UserDTO userDTO) {
+        UserDTO savedUser = userService.saveUser(userDTO);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
 
 
 }
