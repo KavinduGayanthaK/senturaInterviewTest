bb9ca2fbd7a78017d60b487ed9cf8b14a43101a8
Kavindu Gayantha <117292977+KavinduGayanthaK@users.noreply.github.com>
2025-04-04 09:39:29 +0530

    created save user service

--- src/main/java/lk/ijse/senturaInterviewTest/senturaInterviewTest/service/UserService.java
+++ src/main/java/lk/ijse/senturaInterviewTest/senturaInterviewTest/service/UserService.java
@@ -1,10 +1,68 @@
 package lk.ijse.senturaInterviewTest.senturaInterviewTest.service;
 
-import okhttp3.OkHttpClient;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lk.ijse.senturaInterviewTest.senturaInterviewTest.dto.UserDTO;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Map;
 
 @Service
 public class UserService {
 
-private OkHttpClient client;
+    @Autowired
+    private OkHttpClient client;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Value("${api.url}")
+    private String url;
+
+    @Value("${api.key}")
+    private String apikey;
+
+    public UserDTO saveUser(UserDTO userDTO) {
+        String endpoint = url + "/api/users";
+        try {
+
+            String jsonBody = objectMapper.writeValueAsString(userDTO);
+
+            RequestBody body = okhttp3.RequestBody.create(
+                    jsonBody,
+                    MediaType.parse("application/json")
+            );
+
+            Request request = new Request.Builder()
+                    .url(endpoint)
+                    .post(body)
+                    .addHeader("Authorization", "Bearer " + apikey)
+                    .addHeader("Content-Type", "application/json")
+                    .build();
+
+            try (Response response = client.newCall(request).execute()) {
+                if (response.isSuccessful() && response.body() != null) {
+                    String responseBody = response.body().string();
+                    return objectMapper.readValue(responseBody, UserDTO.class);
+                } else {
+                    System.out.println("Failed to save user: " + response.message());
+                    throw new RuntimeException("Failed to save user: " + response);
+                }
+            }
+        } catch (JsonProcessingException e) {
+            System.out.println("Error converting user to JSON");
+            e.printStackTrace();
+            throw new RuntimeException("Error converting user to JSON", e);
+        } catch (IOException e) {
+            System.out.println("Error making HTTP request to  API");
+            e.printStackTrace();
+            throw new RuntimeException("Error making HTTP request to  API", e);
+        }
+    }
+
+
 }
