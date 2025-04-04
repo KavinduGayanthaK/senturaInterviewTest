49cadd1e7b14934c79b365a8de4febb5ccd04732
Kavindu Gayantha <117292977+KavinduGayanthaK@users.noreply.github.com>
2025-04-04 09:30:03 +0530

    created user service

---
+++ src/main/java/lk/ijse/senturaInterviewTest/senturaInterviewTest/service/UserService.java
@@ -0,0 +1,10 @@
+package lk.ijse.senturaInterviewTest.senturaInterviewTest.service;
+
+import okhttp3.OkHttpClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+private OkHttpClient client;
+}
